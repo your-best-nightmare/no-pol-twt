e4aff046818d4698283e66233a98572bdea30c6f
leafdev <dmarjanovic750@gmail.com>
2025-12-01 19:02:36 +0100

    Extension install confirmation added

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-// background.js
-
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension installed and ready to remove Trump/Biden mentions');
+    console.log('Extension installed and ready to remove Trump/Biden mentions');
 });
